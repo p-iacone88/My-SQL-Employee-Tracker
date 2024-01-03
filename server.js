644c081932b5e012d1c08d2ab6f23028529af5cd
[philip iacone] <[philiacone@gmail.com]>
2024-01-03 15:24:46 -0500

    building server.js

--- server.js
+++ server.js
@@ -8,8 +8,41 @@ const mysql = require('mysql2');
 const db = mysql.createConnection(
   {
     host: 'localhost',
+    port: 3306,
     user: 'root',
     password: 'rootroot',
     database: 'employee_tracker_db'
   }
-)
+);
+
+let returnedDbRows = [];
+let returnedInquirerOutput = [];
+
+switch (select) {
+  case "View All Departments":
+    returnedDbRows = db.query(`Select * FROM department`);
+    console.table(returnedDbRows[0]);
+    break;
+
+    case 'View All Roles':
+      returnedDbRows = db.query(`SELECT role.id, role.job_title, role.salary, department.department_name AS department 
+      FROM role 
+      JOIN department ON role.department_id = department.id`);
+      console.table(returnedDbRows[0]);
+      break;
+
+      case 'View All Employees':
+        returnedDbRows = db.query(`SELECT employee.id, employee.first_name, employee.last_name, role.job_title AS title, department.department_name AS department, role.salary AS salary, 
+        CASE WHEN employee.manager_id IS NOT NULL 
+        THEN CONCAT(manager_table.first_name,' ', manager_table.last_name) 
+        ELSE NULL END AS manager 
+        FROM employee 
+        JOIN role 
+        ON employee.role_id = role.id
+        JOIN department ON role.department_id = department.id
+        JOIN employee manager_table ON employee.manager_id = manager_table.id`);
+        console.table(returnedDbRows[0]);
+        break;
+
+}
+
