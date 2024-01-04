eab81d4027c51b518d72e66132843b34b1a53a24
[philip iacone] <[philiacone@gmail.com]>
2024-01-04 15:17:29 -0500

    Added 'view employees by manager'

--- server.js
+++ server.js
@@ -163,48 +163,92 @@ switch (select) {
   break;
 
       case "Update Employee Role":
-        const currentEmployees = await db.query(`
-                SELECT id, first_name, last_name FROM employee;`);
+  try {
+    const [currentEmployees] = await db.query(`
+      SELECT id, first_name, last_name FROM employee;
+    `);
 
-        const currentRoles = await db.query(`
-                SELECT id, job_title FROM role;`);
+    const [currentRoles] = await db.query(`
+      SELECT id, job_title FROM role;
+    `);
 
-        const employeeList = currentEmployees[0].map((employee) => {
-          return {
-            name: `${employee.first_name} ${employee.last_name}`,
-            value: employee.id,
-          };
-        });
+    const employeeList = currentEmployees.map((employee) => {
+      return {
+        name: `${employee.first_name} ${employee.last_name}`,
+        value: employee.id,
+      };
+    });
 
-        const roleList = currentRoles[0].map((role) => {
-          return {
-            name: role.job_title,
-            value: role.id,
-          };
-        });
+    const roleList = currentRoles.map((role) => {
+      return {
+        name: role.job_title,
+        value: role.id,
+      };
+    });
 
-        returnedInquirerOutput = await inquirer.prompt([
-          {
-            type: "list",
-            name: "employeeId",
-            message: "Please Choose Which Employee to Update:",
-            choices: employeeList,
-          },
-          {
-            type: "list",
-            name: "newRole",
-            message: "Please Enter Employee's New Role:",
-            choices: roleList,
-          },
-        ]);
+    returnedInquirerOutput = await inquirer.prompt([
+      {
+        type: "list",
+        name: "employeeId",
+        message: "Please Choose Which Employee to Update:",
+        choices: employeeList,
+      },
+      {
+        type: "list",
+        name: "newRoleId",
+        message: "Please Select Employee's New Role:",
+        choices: roleList,
+      },
+    ]);
 
-        console.log(returnedInquirerOutput);
+    const { employeeId, newRoleId } = returnedInquirerOutput;
 
-        returnedDbRows = await db.query(`
-        UPDATE employee
-        SET role_id = ${returnedInquirerOutput.newRole}
-        WHERE employee.id = ${returnedInquirerOutput.employeeId};`);
-        break;
+    await db.query(
+      `UPDATE employee SET role_id = ? WHERE id = ?`,
+      [newRoleId, employeeId]
+    );
+
+    console.log("Employee role updated successfully!");
+  } catch (error) {
+    console.log("Error occurred:", error);
+  }
+  break;
+
+  case 'View Employees by Manager':
+  returnedInquirerOutput = await inquirer.prompt([
+    {
+      name: 'managerName',
+      message: "Please Enter Manager's Name:",
+    },
+  ]);
+
+  try {
+    const [managerIdResult] = await db.query(
+      `SELECT id FROM employee WHERE CONCAT(first_name, ' ', last_name) = ?`,
+      [returnedInquirerOutput.managerName]
+    );
+
+    if (managerIdResult && managerIdResult.length > 0) {
+      const managerId = managerIdResult[0].id;
+
+      returnedDbRows = await db.query(`
+        SELECT employee.id, employee.first_name, employee.last_name, role.job_title AS title, department.department_name AS department, role.salary AS salary
+        FROM employee
+        JOIN role ON employee.role_id = role.id
+        JOIN department ON role.department_id = department.id
+        WHERE employee.manager_id = ${managerId}
+      `);
+
+      console.table(returnedDbRows[0]);
+    } else {
+      console.log('Manager not found.');
+      // Handle when the manager is not found
+    }
+  } catch (error) {
+    console.log('Error occurred:', error);
+  }
+  break;
+
     }
   } catch (err) {
     console.log(err);
@@ -227,7 +271,8 @@ async function userPrompt() {
           "Add a Department",
           "Add a Role",
           "Add an Employee",
-          "Update an Employee Role",
+          "Update Employee Role",
+          "View Employees by Manager",
           new inquirer.Separator(),
           "Quit",
         ],
